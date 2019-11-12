969beeeb2afbe7d480ac2f8bb42b8a0b599667dc
Josef Hak <jhak@redhat.com>
2019-11-12 11:42:24 +0100

    added 'mode' param (array, join, keys)

---
+++ src/main/java/com/redhat/insights/flattenlistsmt/KeysMode.java
@@ -0,0 +1,13 @@
+package com.redhat.insights.flattenlistsmt;
+
+import org.apache.kafka.connect.data.Schema;
+
+import java.util.List;
+
+public class KeysMode {
+
+    public static Schema buildElementSchema(final List<String> keys) {
+
+        return null;
+    }
+}
